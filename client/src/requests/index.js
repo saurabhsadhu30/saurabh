374402524c6fe95c31fc2784dadb603d3e8a709c
saurabhsadhu30 <99708532+saurabhsadhu30@users.noreply.github.com>
2024-02-22 22:42:18 +0530

    Update index.js

--- client/src/requests/index.js
+++ client/src/requests/index.js
@@ -2,7 +2,7 @@ import Cookies from "js-cookie";
 
 export const server = location.href.includes("localhost")
   ? "http://127.0.0.1:8080"
-  : "https://quizi-zldh.onrender.com";
+  : "https://saurabhmern.onrender.com";
 
 export function config() {
   const token = Cookies.get("token");
